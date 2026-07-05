8a241c485d43ace510290d0d77ef550fee4a4e3d
Isabeli <oliveiraisabeli2007@gmail.com>
2026-07-05 18:33:20 -0300

    adicionando um arquivo js à parte

---
+++ script.js
@@ -0,0 +1,167 @@
+document.addEventListener('DOMContentLoaded', function () {
+            document.getElementById('btn-detalhes-1').addEventListener('click', function () {
+                document.getElementById('card-inner-1').classList.toggle('flip');
+            });
+
+            document.addEventListener('click', function (event) {
+                if (event.target !== document.getElementById('btn-detalhes-1') && event.target !== document.getElementById('card-inner-1')) {
+                    if (document.getElementById('card-inner-1').classList.contains('flip')) {
+                        document.getElementById('card-inner-1').classList.remove('flip');
+                    }
+                }
+            });
+        });
+
+        document.addEventListener('DOMContentLoaded', function () {
+            document.getElementById('btn-detalhes-2').addEventListener('click', function () {
+                document.getElementById('card-inner-2').classList.toggle('flip');
+            });
+
+            document.addEventListener('click', function (event) {
+                if (event.target !== document.getElementById('btn-detalhes-2') && event.target !== document.getElementById('card-inner-2')) {
+                    if (document.getElementById('card-inner-2').classList.contains('flip')) {
+                        document.getElementById('card-inner-2').classList.remove('flip');
+                    }
+                }
+            });
+        });
+
+        document.addEventListener('DOMContentLoaded', function () {
+            document.getElementById('btn-detalhes-3').addEventListener('click', function () {
+                document.getElementById('card-inner-3').classList.toggle('flip');
+            });
+
+            document.addEventListener('click', function (event) {
+                if (event.target !== document.getElementById('btn-detalhes-3') && event.target !== document.getElementById('card-inner-3')) {
+                    if (document.getElementById('card-inner-3').classList.contains('flip')) {
+                        document.getElementById('card-inner-3').classList.remove('flip');
+                    }
+                }
+            });
+        });
+
+        document.addEventListener('DOMContentLoaded', function () {
+            document.getElementById('btn-detalhes-4').addEventListener('click', function () {
+                document.getElementById('card-inner-4').classList.toggle('flip');
+            });
+
+            document.addEventListener('click', function (event) {
+                if (event.target !== document.getElementById('btn-detalhes-4') && event.target !== document.getElementById('card-inner-4')) {
+                    if (document.getElementById('card-inner-4').classList.contains('flip')) {
+                        document.getElementById('card-inner-4').classList.remove('flip');
+                    }
+                }
+            });
+        });
+
+        document.addEventListener('DOMContentLoaded', function () {
+            document.getElementById('btn-detalhes-5').addEventListener('click', function () {
+                document.getElementById('card-inner-5').classList.toggle('flip');
+            });
+
+            document.addEventListener('click', function (event) {
+                if (event.target !== document.getElementById('btn-detalhes-5') && event.target !== document.getElementById('card-inner-5')) {
+                    if (document.getElementById('card-inner-5').classList.contains('flip')) {
+                        document.getElementById('card-inner-5').classList.remove('flip');
+                    }
+                }
+            });
+        });
+
+        document.addEventListener('DOMContentLoaded', function () {
+            document.getElementById('btn-detalhes-6').addEventListener('click', function () {
+                document.getElementById('card-inner-6').classList.toggle('flip');
+            });
+
+            document.addEventListener('click', function (event) {
+                if (event.target !== document.getElementById('btn-detalhes-6') && event.target !== document.getElementById('card-inner-6')) {
+                    if (document.getElementById('card-inner-6').classList.contains('flip')) {
+                        document.getElementById('card-inner-6').classList.remove('flip');
+                    }
+                }
+            });
+        });
+
+        document.addEventListener('DOMContentLoaded', function () {
+            document.getElementById('btn-detalhes-7').addEventListener('click', function () {
+                document.getElementById('card-inner-7').classList.toggle('flip');
+            });
+
+            document.addEventListener('click', function (event) {
+                if (event.target !== document.getElementById('btn-detalhes-7') && event.target !== document.getElementById('card-inner-7')) {
+                    if (document.getElementById('card-inner-7').classList.contains('flip')) {
+                        document.getElementById('card-inner-7').classList.remove('flip');
+                    }
+                }
+            });
+        });
+
+        document.addEventListener('DOMContentLoaded', function () {
+            document.getElementById('btn-detalhes-8').addEventListener('click', function () {
+                document.getElementById('card-inner-8').classList.toggle('flip');
+            });
+
+            document.addEventListener('click', function (event) {
+                if (event.target !== document.getElementById('btn-detalhes-8') && event.target !== document.getElementById('card-inner-8')) {
+                    if (document.getElementById('card-inner-8').classList.contains('flip')) {
+                        document.getElementById('card-inner-8').classList.remove('flip');
+                    }
+                }
+            });
+        });
+
+        document.addEventListener('DOMContentLoaded', function () {
+            document.getElementById('btn-detalhes-9').addEventListener('click', function () {
+                document.getElementById('card-inner-9').classList.toggle('flip');
+            });
+
+            document.addEventListener('click', function (event) {
+                if (event.target !== document.getElementById('btn-detalhes-9') && event.target !== document.getElementById('card-inner-9')) {
+                    if (document.getElementById('card-inner-9').classList.contains('flip')) {
+                        document.getElementById('card-inner-9').classList.remove('flip');
+                    }
+                }
+            });
+        });
+
+        document.addEventListener('DOMContentLoaded', function () {
+            document.getElementById('btn-detalhes-10').addEventListener('click', function () {
+                document.getElementById('card-inner-10').classList.toggle('flip');
+            });
+
+            document.addEventListener('click', function (event) {
+                if (event.target !== document.getElementById('btn-detalhes-10') && event.target !== document.getElementById('card-inner-10')) {
+                    if (document.getElementById('card-inner-10').classList.contains('flip')) {
+                        document.getElementById('card-inner-10').classList.remove('flip');
+                    }
+                }
+            });
+        });
+
+        document.addEventListener('DOMContentLoaded', function () {
+            document.getElementById('btn-detalhes-11').addEventListener('click', function () {
+                document.getElementById('card-inner-11').classList.toggle('flip');
+            });
+
+            document.addEventListener('click', function (event) {
+                if (event.target !== document.getElementById('btn-detalhes-11') && event.target !== document.getElementById('card-inner-11')) {
+                    if (document.getElementById('card-inner-11').classList.contains('flip')) {
+                        document.getElementById('card-inner-11').classList.remove('flip');
+                    }
+                }
+            });
+        });
+
+        document.addEventListener('DOMContentLoaded', function () {
+            document.getElementById('btn-detalhes-12').addEventListener('click', function () {
+                document.getElementById('card-inner-12').classList.toggle('flip');
+            });
+
+            document.addEventListener('click', function (event) {
+                if (event.target !== document.getElementById('btn-detalhes-12') && event.target !== document.getElementById('card-inner-12')) {
+                    if (document.getElementById('card-inner-12').classList.contains('flip')) {
+                        document.getElementById('card-inner-12').classList.remove('flip');
+                    }
+                }
+            });
+        });
